cbb0558fef3678806eebd55ebe5c956f07acd51e
rakhmadikhsanudin <rakhmadikhsanudin@gmail.com>
2012-09-15 02:16:22 +0000

    commit fb kedua

--- New Amikom Social/src/id/ac/amikom/amikomsocial/PostActivity.java
+++ New Amikom Social/src/id/ac/amikom/amikomsocial/PostActivity.java
@@ -1,36 +1,41 @@
 package id.ac.amikom.amikomsocial;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
-
+import com.facebook.android.AsyncFacebookRunner;
+import com.facebook.android.BaseRequestListener;
+import com.facebook.android.Facebook;
+import com.facebook.android.SessionStore;
 import com.markupartist.android.widget.ActionBar;
 import com.markupartist.android.widget.ActionBar.IntentAction;
 
+import android.R.bool;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class PostActivity extends Activity implements LocationListener{
-	
-	protected LocationManager location;
-	private String address;
+public class PostActivity extends Activity {
+
 	private TextView countInfo;
 	private EditText reviewEdit;
-	private Button btnPost;
+
+	private Facebook mFacebook;
+	private CheckBox mFacebookCb;
+	private ProgressDialog mProgress;
+
+	private Handler mRunOnUi = new Handler();
+
+	private static final String APP_ID = "267873336663584";
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -42,15 +47,13 @@ public class PostActivity extends Activity implements LocationListener{
 
 		actionBar.setHomeAction(new IntentAction(this, MainActivity
 				.createIntent(this), R.drawable.ic_action_back));
-		
+
 		actionBar.addAction(new IntentAction(this, new Intent(this,
 				SettingActivity.class), R.drawable.ic_action_share));
-		
+
 		reviewEdit = (EditText) findViewById(R.id.post_txt);
 		countInfo = (TextView) findViewById(R.id.count_id);
-		btnPost = (Button) findViewById(R.id.button_post);
-		
-		
+
 		final TextWatcher mTextEditorWatcher = new TextWatcher() {
 			public void beforeTextChanged(CharSequence s, int start, int count,
 					int after) {
@@ -65,83 +68,94 @@ public class PostActivity extends Activity implements LocationListener{
 			}
 		};
 
-		btnPost.setOnClickListener(new View.OnClickListener() {
-			
-			public void onClick(View view) {
-				Log.i("==Location==", address);
-				
-			}
-		});
-		
 		reviewEdit.addTextChangedListener(mTextEditorWatcher);
 
-		location = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-		
-		Location loc = location.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);		
-		if (loc != null) {						
-		}else{
-			loc = location.getLastKnownLocation(LocationManager.GPS_PROVIDER);			
+		mFacebookCb = (CheckBox) findViewById(R.id.cb_facebook);
+		//mFacebookCb.setChecked(false);
+		mProgress = new ProgressDialog(this);
+
+		mFacebook = new Facebook(APP_ID);
+
+		SessionStore.restore(mFacebook, this);
+
+		if (mFacebook.isSessionValid()) {
+			mFacebookCb.setChecked(true);
+
+			String name = SessionStore.getName(this);
+			name = (name.equals("")) ? "Unknown" : name;
+
+			//mFacebookCb.setText("  Facebook  (" + name + ")");
 		}
-		
-		this.onLocationChanged(loc);
-		
+
+		((Button) findViewById(R.id.button_post))
+				.setOnClickListener(new OnClickListener() {
+					public void onClick(View v) {
+						String review = reviewEdit.getText().toString();
+
+						if (review.equals(""))
+							return;
+
+						if (mFacebookCb.isChecked())
+							postToFacebook(review);
+					}
+				});
+
+		// ((CheckBox) findViewById(R.id.cb_facebook))
+		// .setOnClickListener(new OnClickListener() {
+		// public void onClick(View v) {
+		// if (mFacebook.isSessionValid()) {
+		//
+		// if (mFacebookCb.isChecked()) {
+		// mFacebookCb.setChecked(false);
+		// }
+		// if (!(mFacebookCb.isChecked())) {
+		// mFacebookCb.setChecked(true);
+		// }
+		// } else
+		// mFacebookCb.setChecked(false);
+		//
+		// }
+		// });
 	}
-	
-	public void onLocationChanged(Location loc) {
 
-		double lat = 0;
-		double lon = 0;
+	private void postToFacebook(String review) {
+		mProgress.setMessage("Posting ...");
+		mProgress.show();
 
-		if (loc != null) {
-			lat = loc.getLatitude();
-			lon = loc.getLongitude();
-		}
+		AsyncFacebookRunner mAsyncFbRunner = new AsyncFacebookRunner(mFacebook);
 
-		Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+		Bundle params = new Bundle();
 
-		try {
-			List<Address> addresses = geocoder.getFromLocation(lat, lon, 1);
+		params.putString("message", review);
+//		params.putString("name", "Dexter");
+//		params.putString("caption", "londatiga.net");
+//		params.putString("link", "http://www.londatiga.net");
+//		params.putString(
+//				"description",
+//				"Dexter, seven years old dachshund who loves to catch cats, eat carrot and krupuk");
+//		params.putString("picture", "http://twitpic.com/show/thumb/6hqd44");
 
-			if (addresses != null && addresses.size() > 0) {
-				Address returnedAddress = addresses.get(0);
-				String addr = "";
-				for (int i = 0; i < returnedAddress.getMaxAddressLineIndex(); i++) {
-					addr = addr + " " + returnedAddress.getAddressLine(i);
+		mAsyncFbRunner.request("me/feed", params, "POST",
+				new WallPostListener());
+	}
+
+	private final class WallPostListener extends BaseRequestListener {
+		public void onComplete(final String response) {
+			mRunOnUi.post(new Runnable() {
+				public void run() {
+					mProgress.cancel();
+					Toast.makeText(PostActivity.this, "Posted to Facebook",
+							Toast.LENGTH_SHORT).show();
+
 				}
-				address = addr.trim();				
-				Log.i("==Location==", address);
-
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
+			});
 		}
-
 	}
-	
+
 	public static Intent createIntent(Context context) {
 		Intent i = new Intent(context, PostActivity.class);
 		i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 		return i;
 	}
 
-	public void onProviderDisabled(String arg0) {	
-		
-	}
-
-	public void onProviderEnabled(String arg0) {	
-		
-	}
-
-	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {		
-		
-	}
-	
-	@Override
-	protected void onResume() {
-		super.onResume();
-		location.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000*60*30, 1000, this);
-		location.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000*60*30, 1000, this);
-	}
-
-	
 }
